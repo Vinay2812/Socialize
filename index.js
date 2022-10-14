c5e09717aa7fbb3817e5838d03387f021952b576
Vinay <vinaysarda2812@gmail.com>
2022-10-14 18:07:13 +0530

    Deploy Changes

--- index.js
+++ index.js
@@ -33,14 +33,6 @@ app.use(cors({
     origin: "*",
 }));
 
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-app.use(allowCrossDomain);
 
 //routes
 // app.use("/", async(req, res)=>{res.send("Hello user")});
