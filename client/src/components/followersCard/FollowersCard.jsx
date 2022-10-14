9841214a715c713b682d3db06761d0fd0f276b11
Vinay <vinaysarda2812@gmail.com>
2022-10-14 13:49:22 +0530

    Deploy Changes

--- client/src/components/followersCard/FollowersCard.jsx
+++ client/src/components/followersCard/FollowersCard.jsx
@@ -30,7 +30,7 @@ const Follower = ({other_user})=>{
     return (
         <div className="follower">
             <div>
-                <img src={other_user.profilePicture.url} alt="" className='followerImg'/>
+                <img src={other_user.profilePicture.url} alt="" className='followerImg' crossOrigin/>
                 <Link className="name" to={`/profile/${other_user._id}`}>
                     <span>{other_user.firstname} {other_user.lastname}</span>
                     <span>@{other_user.username}</span>
