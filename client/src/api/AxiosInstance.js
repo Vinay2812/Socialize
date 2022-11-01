bb5c544f8b09ac0c8fe15e1be77bb63324fd588c
Vinay <vinaysarda2812@gmail.com>
2022-11-02 01:17:29 +0530

    Vercel deploy

--- client/src/api/AxiosInstance.js
+++ client/src/api/AxiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios"
-export const API = axios.create({baseURL: "https://socialize-by-vinay.vercel.app/"});
+export const API = axios.create({baseURL: "https://socialize-by-vinay.vercel.app"});
 // export const API = axios.create({baseURL: "https://socialize-by-vinay.herokuapp.com/"});
 // export const API = axios.create({baseURL: "http://localhost:5000/"});
